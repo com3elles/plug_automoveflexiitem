7cd18f166afb5701d397810a90a9f507a31095b8
Berges Yannick <micker@free.fr>
2016-05-11 12:46:14 +0200

    SLQ Query for FLEXIcontent date field

--- automoveflexiitem.php
+++ automoveflexiitem.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die;
 
 class PlgSystemAutomoveflexiitem extends JPlugin
-
+{
     protected $autoloadLanguage = true;
     public function __construct( &$subject, $config )
     {
@@ -22,11 +22,12 @@ class PlgSystemAutomoveflexiitem extends JPlugin
         $state = $this->params->get('changestate', 'nothing');
         $delay = $this->params->get('actiodelay', 'now');
         $cleardate = $this->params->get('cleardate', '1');
-        $limit = 'LIMIT '.$this->params->get('limit', '20');
+        $limit = 'LIMIT '.$this->params->get('limit', '20').'';
+        $fielddateid= $this->params->get('fielddateid','');
         
-        if ($context != 'com_flexicontent.item'){//context
-            return true;
-        }
+       // if ($context != 'com_flexicontent'){//context
+         //   return true;
+        //}
         $serveurdate = date('Y-m-d H:i:s');
        if (function_exists('dump')) dump($serveurdate, 'date serveur');
     }
@@ -41,12 +42,12 @@ class PlgSystemAutomoveflexiitem extends JPlugin
             }
         if (function_exists('dump')) dump($whereCateg, 'catid');
         if ($datemode ==0){
-                $datsource = 'publish_down';
+                $datsource = 'WHERE a.publish_down';
             }else{
-                $datsource = 'publish_down';//TODO requete pour le champ flexicontent
+                $datsource = 'AS a LEFT JOIN #__flexicontent_fields_item_relations AS b ON a.id = b.item_id WHERE b.field_id = '.$fielddateid.'';
             }
             $db = JFactory::getDBO();
-            $query = "SELECT * FROM #__content WHERE ' .$whereCateg.' AND '.$datsource.' < '.$serveurdate.' '.$limit.'";
+            $query = "SELECT a.id, a.title, a.publish_down, b.field_id, b.value , a.catid FROM #__content '.$datsource.' < '.$serveurdate.' AND '.$whereCateg.' '.$limit.'";
             $db->setQuery($query);
             if (function_exists('dump')) dump($query, 'requette');
             $selectarticle = $db->loadObjectList();
